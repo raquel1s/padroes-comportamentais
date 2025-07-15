5f2bbc07b231ffcfd74fed6d260c01a7de103565
raquel1s <silva.raquel2903@gmail.com>
2025-07-14 21:43:50 -0300

    Fazendo a main do command

--- src/main/java/org/example/command/Main.java
+++ src/main/java/org/example/command/Main.java
@@ -1,4 +1,15 @@
 package org.example.command;
 
+import java.util.Stack;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Impressao impressao = new Impressao();
+        Comando comando = new ComandoImprimir(impressao);
+
+        comando.executar();
+        comando.desfazer();
+    }
+
 }
